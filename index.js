adae2e63a17ef2fa8c0a952d94b11b1475c3e37b
Ayhan Huseyin <126552605+ayhanhuseyin@users.noreply.github.com>
2024-05-23 18:50:07 +0400

    Updated

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 
 app.use(express.static('public'))
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'templatemo_552/index.html'))
+    // res.sendFile(path.resolve(__dirname, 'templatemo_552/index.html'))
+    res.render('index');
 })
 
 const port = 3000;
